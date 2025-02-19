08e3bf7010e98d4e64971e3ba11ffa2f9ed3614f
Fr3aky-b <wizkie62@gmail.com>
2025-02-19 13:21:59 +0300

    new

--- shopapp/urls.py
+++ shopapp/urls.py
@@ -1,10 +1,12 @@
 
 from django.contrib import admin
 from django.urls import path
+from setuptools.extern import names
+
 from shopapp import views
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home/', views.home),
-    path('gallary/', views.images),
-    path('about/', views.about),
+    path('home/', views.home, name="home"),
+    path('gallary/', views.images,name="gallary"),
+    path('', views.about,name="about"),
 ]
